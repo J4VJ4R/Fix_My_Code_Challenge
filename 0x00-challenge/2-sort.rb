632c7bc9045a67af5bce799510cc08a2298aa0cb
Javier <ingenierodesoftware73@gmail.com>
2020-09-13 22:02:40 +0000

    upload task 3#

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -8,7 +8,7 @@ result = []
 ARGV.each do |arg|
     # skip if not integer
     next if arg !~ /^-?[0-9]+$/
-
+    
     # convert to integer
     i_arg = arg.to_i
     
@@ -17,7 +17,7 @@ ARGV.each do |arg|
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] > i_arg
+        if result[i] < i_arg
             i += 1
         else
             result.insert(i, i_arg)
@@ -26,7 +26,6 @@ ARGV.each do |arg|
         end
     end
     result << i_arg if !is_inserted
-
 end
 
 puts result
